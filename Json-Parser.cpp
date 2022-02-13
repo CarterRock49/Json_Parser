047fac8604b7e67c9a9512d54b543a4841a912c0
CarterRock49 <CarterRock49@gmail.com>
2022-02-13 14:18:36 -0500

    Replaced array with map of vectors

--- Json-Parser.cpp
+++ Json-Parser.cpp
@@ -2,7 +2,9 @@
 #include <fstream>
 #include <cctype>
 #include <string>
-#include <sstream> 
+#include <sstream>
+#include <map>
+#include <vector>
 using namespace std;
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
@@ -17,9 +19,8 @@ private:
 
 
 public:
-	string names[ARRSIZE];
-	string values[ARRSIZE];
-	string array_values[ARRSIZE];
+	map<string, vector<string>> nNamesValues;
+	string tempstorage;
 	//member function takes in file name as a parameter, opens the file, and returns if it was successful
 	bool OpenFile(string fileDIR) {
 		//opens the file that the fileDIR varible has stored in it
@@ -34,10 +35,11 @@ public:
 	}
 
 	bool ParseJson() {
-		string entry;
+		cout<<"you are here";
+		string jsonstring;
+		string tempvalue;
+		vector<string> value;
 		char charentry;
-		int index = 0;
-		int arrayindex = 0;
 		//gets you to to first string for the loop
 		Json.get(charentry);
 		//the ws function removes as many whitespace characters as possible
@@ -46,15 +48,17 @@ public:
 		//this is a while loop that continues until the end of the file
 			while (!Json.eof()) {
 				Json >> ws;
+				if(Json.peek() == ','){Json.get(charentry);} //gets rid of ',' to start
 				if(Json.peek() == '"'){Json.get(charentry);} //gets rid of '"' to start
-				getline(Json, entry, '"');	//gets the first string
-				names[index] = entry;	
-				getline(Json, entry, ':'); //removes anything up to and including the colon
+				getline(Json, jsonstring, '"');	//gets the first string
+				getline(Json, tempstorage, ':'); //removes anything up to and including the colon
 				Json >> ws; //if the next char is a '"' thens its a string and will end with a '"' else its something else and will end with either a ',' or '\n' and the '}' or its an array
 				if (Json.peek() == '"') {
 					Json.get(charentry); //gets rid of the '"'
-					getline(Json, entry, '"');
-					values[index] = entry;
+					getline(Json, tempvalue, '"');
+					value.push_back(tempvalue);
+					nNamesValues.insert(pair<string, vector<string>> (jsonstring, value));
+					value.clear();
 					Json.get(charentry); //gets rid of the ',' or '\n'
 				}
 				//check if the next character is for the start of a json array
@@ -64,97 +68,95 @@ public:
 					//this is a bit messy but this while loop continues until the ] is reached for the array then it uses the break statement
 					while(true){
 						Json >> ws;
-						if(Json.peek() == ']'){Json.get(charentry); break;}
-						//the same code for the outer loop that parses if a value is a string or a number
+						if(Json.peek() == ']'){Json.get(charentry); nNamesValues.insert(pair<string, vector<string>> (jsonstring, value)); value.clear(); break;}
+						//the same code for the outer loop that parses if a value is a string or an int
 						if (Json.peek() == '"'){
 							Json.get(charentry);
-							getline(Json, entry, '"');
-							array_values[arrayindex] = entry;
+							getline(Json, tempvalue, '"');
+							value.push_back(tempvalue);
 							Json.get(charentry);
 						} else {
-							getline(Json, entry);
-							stringstream ss(entry);
-							getline(ss, entry, ',');
-							array_values[arrayindex] = entry;
+							getline(Json, tempvalue);
+							stringstream ss(tempvalue);
+							getline(ss, tempvalue, ',');
+							value.push_back(tempvalue);
 						}
-						arrayindex++;
 					}
 				} else {
 					//this grabs the entire value portion, this is done to not accdentially grab the } at the end of the file although downside, doing it grabs the ,
-					getline(Json, entry);
+					getline(Json, tempvalue);
 					//this puts entry into a stream
-					stringstream ss(entry);
+					stringstream ss(tempvalue);
 					//this removes the , but because we only grabbed this line before it doesn't have the possiblity to grabs the final }
-					getline(ss, entry, ',');
-					values[index] = entry;
+					getline(ss, tempvalue, ',');
+					value.push_back(tempvalue);
+					nNamesValues.insert(pair<string, vector<string>> (jsonstring, value));
+					value.clear();
 				}
-				//interates on the variable 'index' to change the parallel array's row
-				index++;
 			}
 		return true;
+	}
+
+	string GetValueGivenKey(string jsonstring){
+		cout<<"you are here23";
+		vector<string> value;
+		string finalstring;
+		value = nNamesValues.find(jsonstring)->second;
+			for (int index = 0; index < value.size(); index++){
+				if(index > 0){finalstring.append(",");}
+				finalstring.append(value[index]);
+			}
+		return finalstring;
 	}
 };
 ///////////////// Testing ////////////////////////////////
 TEST_CASE("Testing my JSON Parser"){
 	JsonParser jp;
-	const string fileDIR1 = "JsonObject1.json";
-	const string fileDIR2 = "JsonObject2.json";
 
 	SUBCASE("testing parsing of empty JSON object") {
 		// Open the file
-		CHECK(jp.OpenFile(DATA_FILE_PATH + "JsonObject4a.json") == true);
+		CHECK(jp.OpenFile(DATA_FILE_PATH + "JsonObject5a.json") == true);
 
 		// Invoke the parsing process
 		CHECK(jp.ParseJson() == true);
 
 		// Validate the parsed JSON object
-		CHECK(jp.names[0] == "Rank");
-		CHECK(jp.names[1] == "Name");
-		CHECK(jp.names[2] == "Symbol");
-		CHECK(jp.names[3] == "Market Cap");
-		CHECK(jp.values[0] == "1");
-		CHECK(jp.values[1] == "Bitcoin");
-		CHECK(jp.values[2] == "BTC");
-		CHECK(jp.array_values[0] == "");
+		CHECK(jp.GetValueGivenKey("Rank") == "1");
+		CHECK(jp.GetValueGivenKey("Active") == "false");
+		CHECK(jp.GetValueGivenKey("Name") == "Bitcoin");
+		CHECK(jp.GetValueGivenKey("Symbol") == "BTC");
+		CHECK(jp.GetValueGivenKey("Market Cap") == "1000,0,-12345.67890e-789");
 	}
 
 	SUBCASE("testing parsing of simple JSON object (only strings for values)") {
 		// Open the file
-		CHECK(jp.OpenFile(DATA_FILE_PATH + "JsonObject4b.json") == true);
+		CHECK(jp.OpenFile(DATA_FILE_PATH + "JsonObject5b.json") == true);
 
 		// Invoke the parsing process
 		CHECK(jp.ParseJson() == true);
 
 		// Validate the parsed JSON object
-		CHECK(jp.names[0] == "Rank");
-		CHECK(jp.names[1] == "Name");
-		CHECK(jp.names[2] == "Symbol");
-		CHECK(jp.names[3] == "Market Cap");
-		CHECK(jp.values[0] == "21");
-		CHECK(jp.values[1] == "Algorand");
-		CHECK(jp.values[2] == "ALGO");
-		CHECK(jp.array_values[0] == "One Thousand");
-		CHECK(jp.array_values[1] == "Two Thousand");
-		CHECK(jp.array_values[2] == "Three Thousand");
+		CHECK(jp.GetValueGivenKey("Rank") == "1");
+		CHECK(jp.GetValueGivenKey("Name") == "Bitcoin");
+		CHECK(jp.GetValueGivenKey("Symbol") == "BTC");
+		CHECK(jp.GetValueGivenKey("Market Cap") == "1000,0,-12345.67890e-789");
+		CHECK(jp.GetValueGivenKey("Last Active") == "true,false,true");
+
 	}
 
 	SUBCASE("testing parsing of simple JSON object (only strings for values)") {
 		// Open the file
-		CHECK(jp.OpenFile(DATA_FILE_PATH + "JsonObject4c.json") == true);
+		CHECK(jp.OpenFile(DATA_FILE_PATH + "JsonObject5c.json") == true);
 
 		// Invoke the parsing process
 		CHECK(jp.ParseJson() == true);
 
 		// Validate the parsed JSON object
-		CHECK(jp.names[0] == "Rank");
-		CHECK(jp.names[1] == "Name");
-		CHECK(jp.names[2] == "Symbol");
-		CHECK(jp.names[3] == "Market Cap");
-		CHECK(jp.values[0] == "1");
-		CHECK(jp.values[1] == "Bitcoin");
-		CHECK(jp.values[2] == "BTC");
-		CHECK(jp.array_values[0] == "1000");
-		CHECK(jp.array_values[1] == "0");
-		CHECK(jp.array_values[2] == "-12345.67890e-789");
+		CHECK(jp.GetValueGivenKey("Rank") == "1");
+		CHECK(jp.GetValueGivenKey("Name") == "Bitcoin");
+		CHECK(jp.GetValueGivenKey("Symbol") == "BTC");
+		CHECK(jp.GetValueGivenKey("Market Cap") == "1000,0,-12345.67890e-789");
+		CHECK(jp.GetValueGivenKey("Last Active") == "true, false,true");
+		CHECK(jp.GetValueGivenKey("Last Sold") == "null");
 	}
 }
